4d0816761e523bfaf6f9358b812e01923f7321e5
propenster <faitholusegun60@gmail.com>
2020-10-03 21:36:41 +0100

    updated appsettings.json

---
+++ AircraftMonitorAPI.DataAccess/Repositories/ICardixPatientRepository.cs
@@ -0,0 +1,19 @@
+﻿using AircraftMonitorAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AircraftMonitorAPI.DataAccess.Repositories
+{
+    public interface ICardixPatientRepository
+    {
+        Task<IEnumerable<CardixPatient>> GetAllCardixPatientsAsync();
+        Task<CardixPatient> GetCardixPatientById(int Id);
+        Task<CardixPatient> GetCardixPatientByPatientId(string PatientId);
+        void AddCardixPatient(CardixPatient patient);
+        void UpdateCardixPatient(CardixPatient patient);
+        void DeleteCardixPatient(int Id);
+
+    }
+}
