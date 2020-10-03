e14f12b7c136cf3ee255c5ac7d64e7f6c487abd7
propenster <faitholusegun60@gmail.com>
2020-10-03 21:46:04 +0100

    dsdd

--- AircraftMonitorAPI.Services/ICardixPatientService.cs
+++ AircraftMonitorAPI.Services/ICardixPatientService.cs
@@ -8,5 +8,12 @@ namespace AircraftMonitorAPI.Services
     public interface ICardixPatientService
     {
         Task<IEnumerable<CardixPatient>> GetAllCardixPatients();
+        Task<CardixPatient> GetCardixPatientById(int Id);
+        Task<CardixPatient> GetCardixPatientBypatientId(string PatientId);
+        void AddCardixPatient(CardixPatient patient);
+        void UpdateCardixPatient(CardixPatient patient);
+        void DeleteCardixPatient(string Id);
+
+
     }
 }
