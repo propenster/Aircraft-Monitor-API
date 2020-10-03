7b00ac65d4a04e09caeab427fa93078589c34ae8
propenster <faitholusegun60@gmail.com>
2020-10-03 21:42:30 +0100

    Added Services Class Lib

---
+++ AircraftMonitorAPI.Services/ICardixPatientService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AircraftMonitorAPI.Services
+{
+    public interface ICardixPatientService
+    {
+        Task<IEnumerable<CardixPatient>> GetAllCardixPatients();
+    }
+}
